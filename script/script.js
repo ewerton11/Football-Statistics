17718c037c705265e5a5b7953047dd9ed19ef75c
ewerton11 <ewertonrc01@gmail.com>
2022-09-29 17:41:28 -0300

    inicio do script

---
+++ script/script.js
@@ -0,0 +1,8 @@
+menu = document.querySelector('menu-box-1');
+logo = document.querySelector('.box-2');
+menuN = document.querySelector('.nav-menu');
+
+
+logo.addEventListener('click' , ()=> {
+    document.location.reload(true);
+});
