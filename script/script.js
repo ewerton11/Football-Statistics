2e0b0fc09dea4771fce8923fd7f9d5507e184e5c
ewerton11 <ewertonrc01@gmail.com>
2022-10-03 17:44:32 -0300

    abrir e fechar menu

--- script/script.js
+++ script/script.js
@@ -1,8 +1,21 @@
-menu = document.querySelector('menu-box-1');
-logo = document.querySelector('.box-2');
-menuN = document.querySelector('.nav-menu');
+let menu = document.querySelector('.menu-box-1')
+let navmenu = document.querySelector('#nav-menu')
+let xmenu = document.querySelector('#x-menu')
+let logo = document.querySelector('.box-2');
+
+let count = 0;
+
+
 
 
 logo.addEventListener('click' , ()=> {
     document.location.reload(true);
 });
+
+menu.addEventListener('click' , ()=> {
+    navmenu.classList.toggle("active");
+});
+
+xmenu.addEventListener('click' , ()=> {
+    navmenu.classList.toggle("active");
+})
